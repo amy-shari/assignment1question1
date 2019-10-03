c24ab8debee1fc59d54f44d70b99eacec57001ae
amy-shari <56041534+amy-shari@users.noreply.github.com>
2019-10-03 11:22:17 -0500

    Add files via upload

---
+++ a1q1.java
@@ -0,0 +1,47 @@
+/*******************************************************
+ * Name:        Amy Mann
+ * Class:       CS20S - 02
+ * 
+ * Assignment:  A1 Q1
+ * 
+ * Description: A list five places I have visited in my life (on separate lines).
+ **********************************************************************/
+
+// import statements go here
+
+public class a1q1 {
+    public static void main(String[] args) {
+    // ***** Declaration of Constants *****
+    
+    // ***** Declaration of Variables *****
+    
+    // ***** Create Objects *****
+    
+    // ***** Output Banners *****
+    
+        System.out.println("***************************");
+        System.out.println("Name: Amy M");
+        System.out.println("Class: CS20 - 02");
+        System.out.println("Assignment: A1 Q1");
+        System.out.println("***************************\n");
+   
+    // ***** Get input *****
+   
+    // ***** Processing *****
+   
+    // ***** Output *****
+   
+        System.out.println("Buffalo, U.S.A");
+        System.out.println("Munich, Germany");
+        System.out.println("Florence, Italy");
+        System.out.println("Versailles, France");
+        System.out.println("Calgary, Canada");
+    
+    // ***** Closing Message *****
+    
+        System.out.println();
+        System.out.println("End of processing");
+   
+    } // end of main method
+    
+} // end of public class
